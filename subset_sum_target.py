bc2f650081cabf2158cf0ff71cd75ab9df12ec03
MariaAbh <maria.abouhaidar1@gmail.com>
2023-11-21 18:04:32 +0100

    Minimize function calls

--- subset_sum_target.py
+++ subset_sum_target.py
@@ -1,4 +1,9 @@
+# counter = 0
+
 def wrap_subset_sum(l,target):
+	# global counter
+	# counter += 1
+
 	if target == 0:
 		return ([],True)
 	if target < 0 or len(l) == 0:
@@ -6,11 +11,10 @@ def wrap_subset_sum(l,target):
 	
 	t_sub = wrap_subset_sum(l[1:],target-l[0])
 	do_take, val_do = [l[0]]+t_sub[0],t_sub[1]
-	no_take, val_no = wrap_subset_sum(l[1:],target)
-	
 	if val_do == True:
 		return do_take, True
-	elif val_no == True:
+	no_take, val_no = wrap_subset_sum(l[1:],target)
+	if val_no == True:
 		return no_take, True
 	else:
 		return [],False
@@ -22,3 +26,4 @@ def subset_sum(l,t):
 l = [12,1,61,5,9,2]
 t = 24
 print(subset_sum(l,t))
+# print(counter)
