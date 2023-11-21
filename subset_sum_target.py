a3283da23a47149b532b21315cbe400a533d7dc3
MariaAbh <maria.abouhaidar1@gmail.com>
2023-11-21 17:26:41 +0100

    Add solution to subset sum problem

---
+++ subset_sum_target.py
@@ -0,0 +1,24 @@
+def subset_sum(l,target):
+	if target == 0:
+		return ([],True)
+	if target < 0 or len(l) == 0:
+		return ([],False)
+	
+	t_sub = subset_sum(l[1:],target-l[0])
+	do_take, val_do = [l[0]]+t_sub[0],t_sub[1]
+	no_take, val_no = subset_sum(l[1:],target)
+	
+	if val_do == True:
+		return do_take, True
+	elif val_no == True:
+		return no_take, True
+	else:
+		return [],False
+
+def wrap_subset(l,t):
+	l_res, t_res = subset_sum(l,t)
+	return l_res
+
+l = [12,1,61,5,9,2]
+t = 24
+print(wrap_subset(l,t))
